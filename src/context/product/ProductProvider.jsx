30eaefae524fe72ecb93f294f3892ff39aaf557a
Sebastian <scubillosq@gmail.com>
2023-04-16 23:20:02 -0400

    feature se comienza a trabajar en el contecto, provider y reducer de productos

--- src/context/product/ProductProvider.jsx
+++ src/context/product/ProductProvider.jsx
@@ -1,10 +1,30 @@
-import ProductContext from "./ProductContext"
+import ProductContext from "./ProductContext";
+import { useReducer } from "react";
+import productReducer from "./productReducer";
 
 const ProductProvider = ({children}) => {
 
-    const msg = "Hola Muchacho!";
+    const initialState = {
+        products: [],
+        product: [{
+            id: "",
+            albumName: "",
+            bandName: "",
+            price:"" ,
+            format: "",
+            img: "",
+            SKU: "",
+            stock: ""
+        }]
+    }
+
+    const [productState, dispatch] = useReducer(x, initialState);
+
+    const getProducts = async() => {
+        const products = await axiosClient.get("/products")
+    }
     return(
-        <ProductContext.Provider value={{msg}}>{children}</ProductContext.Provider>
+        <ProductContext.Provider value={{}}>{children}</ProductContext.Provider>
     )
 }
 
---
+++ src/context/product/productReducer.jsx
@@ -0,0 +1,31 @@
+const productReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case "GET_PRODUCTS":
+            return {
+                ...state,
+                product: payload,
+                product: [{
+                    id: "",
+                    albumName: "",
+                    bandName: "",
+                    price:"" ,
+                    format: "",
+                    img: "",
+                    SKU: "",
+                    stock: ""
+                }]
+            }
+        case "GET_PRODUCT":
+            return {
+                ...state,
+                product: [payload]
+            }
+        default:
+            return state;
+        
+    }
+}
+
+export default productReducer;
