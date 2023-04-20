e831922a916b5f44c834157303336abb52be88c6
Sebastian <scubillosq@gmail.com>
2023-04-20 17:16:19 -0400

    Feature Se añade el Footer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../../routes/navigation/Navigation";
+import Footer from "../footer/Footer";
 
 const Layout = () => {
   return (
     <>
         <Navigation />
         <Outlet />
+        <Footer />
     </>
   )
 }
