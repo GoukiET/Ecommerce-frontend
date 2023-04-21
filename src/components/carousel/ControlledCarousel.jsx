b571945024d311a5b18a55d4428706bfbcb86052
Sebastian <scubillosq@gmail.com>
2023-04-21 12:40:04 -0400

    Feature Se añaden más componentes al carusel

--- src/components/carousel/ControlledCarousel.jsx
+++ src/components/carousel/ControlledCarousel.jsx
@@ -3,7 +3,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import Overkill from '../../assets/images/Overkill.webp';
 import Exodus from '../../assets/images/Exodus.webp';
 import Immortal from '../../assets/images/Immortal.webp';
-
+import Fangels from '../../assets/images/Fangels.webp';
+import ScarSymmetry from '../../assets/images/ScarSymmetry.webp';
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -33,6 +34,20 @@ function ControlledCarousel() {
           <img
             className="d-block w-100"
             src={Immortal}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Fangels}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={ScarSymmetry}
             alt="Third slide"
           />
         </Carousel.Item>
