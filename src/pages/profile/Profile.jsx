bd04d4bbde2c42215be420906353fa8b7d1b564f
Sebastian <scubillosq@gmail.com>
2023-04-21 15:51:28 -0400

    Feature se añade el profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,17 +1,43 @@
+
 import UserContext from "../../context/user/UserContext";
 import { useContext } from "react";
+import { Card, CardContent, CardMedia, Typography, Container } from "@mui/material";
+
 
 const Profile = () => {
 
-  const {infoUser, userEdit} = useContext(UserContext);  
+  const {infoUser} = useContext(UserContext);  
 
 
   const {name, lastName, email, isAdmin} = infoUser;
   return (
-    <div>
-        <h1>{name +' '+ lastName}</h1>
-        <h3>{email}</h3>
-        <p>{isAdmin ? "Administrador" : "Cliente"}</p>
+    <div>        
+        <Container sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        marginTop: "140px",
+        marginBottom: "140px"
+      }}>
+        <Card sx={{ maxWidth: 500}}>
+          <CardMedia sx={{ height: 250, width: 500}} image={isAdmin ? "https://www.gizcomputer.com/wp-content/uploads/2021/10/Que-hace-un-Administrador-de-Sistemas-Informaticos-en-Red.jpg" : "https://static.vecteezy.com/system/resources/previews/001/822/712/non_2x/man-buying-something-online-free-photo.jpg"} title="Hombre con cigarro" />
+          <CardContent>
+          <Typography
+              variant="h6"
+              align="center"
+              color="text.secondary"
+            >
+              Nombre: {name}
+            </Typography>
+            <Typography variant="h6" color="text.secondary" align="center">
+              Email: {email}
+            </Typography>
+            <Typography variant="h6" color="text.secondary" align="center">
+              Rol: {isAdmin ? "Administrador" : "Cliente"}
+            </Typography>
+          </CardContent>         
+        </Card>
+      </Container>      
     </div>
   )
 }
