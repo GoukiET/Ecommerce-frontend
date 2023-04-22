cec2f7f272c4f73f912247243454c1d251f61c22
Sebastian <scubillosq@gmail.com>
2023-04-21 22:34:40 -0400

    Feature Correción link en cardProduct

--- src/components/cardProducts/CardProducts.jsx
+++ src/components/cardProducts/CardProducts.jsx
@@ -1,43 +1,43 @@
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-
-
-const CardProducts = ({product}) => {
-    
-    const {_id, bandName, albumName, img, format, stock } = product;
-
+const CardProducts = ({ product }) => {
+  const { _id, bandName, albumName, img, format, stock } = product;
 
   return (
     <div>
-        <Link to={`/product/${_id}`}>
-          <div className="group relative">
-            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+      <Link to={`/product/${_id}`} style={{ textDecoration: "none" }}>
+        <div className="group relative">
+          <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
             <img
-                src={img}
-                alt="image"
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-center">
-              <div>
-                <h3 className="text-sm text-gray-700">{bandName} - {albumName}</h3>                
-              </div>       
-            </div>
+              src={img}
+              alt="image"
+              className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+            />
+          </div>
+          <div className="mt-4 flex justify-center">
             <div>
-            <p className="flex text-sm font-medium text-gray-900 justify-center">
-                {format}
-              </p>
+              <h3 className="text-sm text-gray-700">
+                {bandName} - {albumName}
+              </h3>
             </div>
           </div>
-          <div className="flex justify-center">
-          {stock === 0 ? <h5 className="text-red-900">Sin Stock</h5> :  <Button>comprar</Button>}
+          <div>
+            <p className="flex text-sm font-medium text-gray-900 justify-center">
+              {format}
+            </p>
           </div>
-          
-        </Link>
-        
-      </div>
-  )
-}
+        </div>
+        <div className="flex justify-center">
+          {stock === 0 ? (
+            <h5 className="text-red-900">Sin Stock</h5>
+          ) : (
+            <Button>comprar</Button>
+          )}
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default CardProducts
+export default CardProducts;
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -2,34 +2,33 @@ import ProductContext from "../../context/product/ProductContext";
 import { useContext, useEffect } from "react";
 import CardProducts from "../../components/cardProducts/CardProducts";
 
-
 const Products = () => {
-    const {getProducts, products} = useContext(ProductContext);
+  const { getProducts, products } = useContext(ProductContext);
 
-    useEffect(() => {
-        const fetchProducts = async() => {
-            await getProducts()
-        }
-        fetchProducts()
-    }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      await getProducts();
+    };
+    fetchProducts();
+  }, []);
 
-    return (
-        <div>
-            <div className="bg-white">
-                <div className="max-w-2x1 mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7x1 lg:pg-8">
-                    <h2 className="text-2x1 font-extrabold tracking-tight text-gray-900">
-                        Nuestro Catálogo
-                    </h2>
+  return (
+    <div>
+      <div className="bg-white">
+        <div className="max-w-2x1 mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7x1 lg:pg-8">
+          <h2 className="text-2x1 font-extrabold tracking-tight text-gray-900">
+            Nuestro Catálogo
+          </h2>
 
-                    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        {products.map((product) => (
-                            <CardProducts key={product._id} product={product} />
-                        ))}
-                    </div>
-                </div>
-            </div>
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <CardProducts key={product._id} product={product} />
+            ))}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Products;
