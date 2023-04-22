5635b3c6ce60cd451124c3431efd328f8609a4b1
Sebastian <scubillosq@gmail.com>
2023-04-21 20:54:47 -0400

    Feature se modifica import de paypal

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -3,7 +3,7 @@ import "./Checkout.scss";
 import { useContext } from "react";
 import CartContext from "../../context/cart/CartContext";
 import CheckoutItem from "../../components/checkout_item/CheckoutItem";
-import Paypal from "../../components/paypal";
+import Paypal from "../../components/Paypal";
 
 const Checkout = () => {
 
